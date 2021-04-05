0f9981cf8120fe0ac70d2fcab0f4291adf1a174a
Matt Walker <matthew.walker@postmates.com>
2021-04-05 14:51:50 -0700

    move out of folder

--- src/components/SignUpFlow/SignUpFlow.js
+++ src/components/SignUpFlow/SignUpFlow.js
@@ -6,10 +6,10 @@ import Section from "components/Section";
 import Container from "@material-ui/core/Container";
 import SectionHeader from "components/SectionHeader";
 import ProgressBarSection from "components/SignUpFlow/ProgressBarSection";
-import StepSignin from "components/Signupflowsteps/Stepsignin";
-import StepTwo from "components/Signupflowsteps/StepTwo";
-import StepThree from "components/Signupflowsteps/StepThree";
-import StepFour from "components/Signupflowsteps/StepFour";
+import StepSignin from "components/Stepsignin";
+import StepTwo from "components/StepTwo";
+import StepThree from "components/StepThree";
+import StepFour from "components/StepFour";
 import { FormContainer } from "styles/Styles";
 
 function SignUpFlow(props) {
